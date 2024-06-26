eb5a2647574cfff0ef1d9e7b21877304e28c0a9e
SLMCreatives <sulaiman@slmcreatives.com>
2024-06-26 16:20:51 +0800

    commit.2.26.6

--- app/archive/page.tsx
+++ app/archive/page.tsx
@@ -7,8 +7,9 @@ import Image from "next/image";
 
 import { client, sanityFetch } from "../sanity/lib/client";
 import { SanityImageSource } from "@sanity/image-url/lib/types/types";
+import { ArrowRightIcon } from "@heroicons/react/24/outline";
 
-const POSTS_QUERY = `*[_type == "post"]{title, body, slug, date, mainImage, publishedAt}|order(date desc)`;
+const POSTS_QUERY = `*[_type == "post"]{title, body, author->, slug, date, mainImage, publishedAt, categories[]->, category->}|order(date desc)`;
 const urlFor = (source: SanityImageSource) =>
   imageUrlBuilder(client).image(source).auto("format").fit("max").url();
 const dateFormated = (publishedAt: string) => {
@@ -26,7 +27,7 @@ export default async function IndexPage() {
     <main>
       <Header />
 
-      <div className="flex justify-center items-center bg-white min-h-screen-1/2">
+      <div className="flex justify-center items-center bg-white min-h-screen-1/3">
         <div className="relative isolate px-6 pt-14 lg:px-8">
           <div
             className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
@@ -49,52 +50,43 @@ export default async function IndexPage() {
                 Get the latest trends and valuable digital marketing tips.
               </p>
             </div>
-            <div className="mt-10 flex items-center justify-center gap-x-6">
-              <a
-                href="/form"
-                className="rounded-md bg-emerald-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-emerald-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-600"
-              >
-                Get FREE Quotation
-              </a>
-
-              <a
-                href="#elements"
-                rel="noreferrer"
-                className="scroll-behavior-smooth text-sm font-semibold leading-6 text-gray-900"
-                id="scroll"
-              >
-                Learn more <span aria-hidden="true">→</span>
-              </a>
-            </div>
           </div>
         </div>
       </div>
       <div className="flex flex-col gap-12 px-4 lg:px-32 py-8 bg-white justify-center items-center">
-        <ul className="grid grid-cols-1 gap-12 lg:grid-cols-3">
+        <ul className="grid grid-cols-1 gap-6 lg:grid-cols-2">
           {posts.map((post) => (
             <li
-              className="bg-slate-100 shadow-sm hover:bg-slate-50 hover:shadow-lg p-8 rounded-lg"
+              className="bg-white shadow-sm items-center justify-center lg:justify-left hover:bg-slate-50 hover:shadow-lg p-8 lg:p-8 rounded-lg"
               key={post._id}
             >
               <Link
                 className="text-emerald-600 visited:text-emerald-600 "
                 href={`/archive/posts/${post?.slug.current}`}
               >
-                <Image
-                  className="w-full h-60 w-full mr-6 mb-4 object-cover rounded-lg"
-                  src={urlFor(post.mainImage)}
-                  alt={post.title}
-                  width={500}
-                  height={500}
-                />
-                <h2 className="text-xl font-semibold">{post?.title}</h2>
-                <p className="text-gray-500 text-sm mt-2">
-                  {dateFormated(post.publishedAt)}
-                </p>
-
-                <p className="text-gray-500 mt-2 text-sm ">
-                  {post?.body[0].children[0].text}
-                </p>
+                <div className="flex flex-row items-center justify-left">
+                  <Image
+                    className="hidden lg:block lg:h-32 lg:w-32 mr-6 object-cover rounded-lg"
+                    src={urlFor(post.mainImage)}
+                    alt={post.title}
+                    width={500}
+                    height={500}
+                  />
+                  <h2 className="text-xl text-gray-800 lg:text-3xl my-4 leading-2 font-semibold">
+                    {post?.title}
+                    <p className="text-gray-500 text-sm font-light mt-2">
+                      {post?.categories[0].title} -{" "}
+                      {dateFormated(post.publishedAt)}
+                    </p>
+                    <a
+                      href={`/archive/posts/${post?.slug.current}`}
+                      className="text-emerald-600 visited:text-emerald-600 text-sm"
+                    >
+                      Read Now
+                      <ArrowRightIcon className="inline text-emerald-600 ml-4 w-4 h-4 my-auto" />
+                    </a>
+                  </h2>
+                </div>
               </Link>
             </li>
           ))}
