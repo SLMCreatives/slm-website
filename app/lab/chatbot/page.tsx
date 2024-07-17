dd74615260db66da92f9cec8b7a338fe8619f3f0
SLMCreatives <sulaiman@slmcreatives.com>
2024-07-17 19:27:20 +0800

    chatbot UI

--- app/lab/chatbot/page.tsx
+++ app/lab/chatbot/page.tsx
@@ -12,49 +12,50 @@ export default function Home() {
   const [input, setInput] = useState<string>("");
 
   return (
-    <div className="flex flex-col bg-slate-900 h-screen justify-between items-center">
-      <div className="flex flex-col gap-4 p-4 text-white">
-        {conversation.map((message, index) => (
-          <div key={index}>
-            {message.role}: {message.content}
-          </div>
-        ))}
+    <main>
+      <div className="flex p-6 justify-center bg-black">
+        <h1 className="text-white font- text-4xl">Gemini Chatbot</h1>
       </div>
-
-      <div className="fixed bottom-5 flex gap-4 justify-center items-stretch w-full">
-        <input
-          className="border rounded p-2"
-          type="text"
-          value={input}
-          onChange={(event) => {
-            setInput(event.target.value);
-          }}
-        />
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          onClick={async () => {
-            const { messages, newMessage } = await continueConversation([
-              ...conversation,
-              { role: "user", content: input },
-            ]);
-
-            let textContent = "";
-
-            for await (const text of readStreamableValue(newMessage)) {
-              textContent = `${textContent}${text}`;
-
-              setConversation([
-                ...messages,
-                { role: "assistant", content: textContent },
+      <div className="flex flex-col [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)] h-screen items-center">
+        <div className="flex flex-col gap-4 p-4 text-white w-[70vw]">
+          {conversation.map((message, index) => (
+            <div key={index} className="grid grid-cols-4">
+              <p className="font-semibold capitalize">{message.role}:</p>
+              <p className="text-md col-span-3">{message.content}</p>
+            </div>
+          ))}
+        </div>
+        <div className="fixed bottom-5 flex gap-4 justify-center w-[100vw]">
+          <input
+            className="border rounded p-2 w-[70vw]"
+            type="text"
+            value={input}
+            onChange={(event) => {
+              setInput(event.target.value);
+            }}
+          />
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={async () => {
+              const { messages, newMessage } = await continueConversation([
+                ...conversation,
+                { role: "user", content: input },
               ]);
-
-              setInput("");
-            }
-          }}
-        >
-          Send Message
-        </button>
+              let textContent = "";
+              for await (const text of readStreamableValue(newMessage)) {
+                textContent = `${textContent}${text}`;
+                setConversation([
+                  ...messages,
+                  { role: "assistant", content: textContent },
+                ]);
+                setInput("");
+              }
+            }}
+          >
+            Send Message
+          </button>
+        </div>
       </div>
-    </div>
+    </main>
   );
 }
