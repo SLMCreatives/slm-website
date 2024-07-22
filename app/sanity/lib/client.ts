54c45c433d3b5929a9e16d288f490e34779676c3
SLMCreatives <sulaiman@slmcreatives.com>
2024-07-22 09:02:58 +0800

    tags

--- app/sanity/lib/client.ts
+++ app/sanity/lib/client.ts
@@ -12,14 +12,17 @@ export async function sanityFetch<QueryResponse>({
   query,
   params = {},
   revalidate = 60, // default revalidation time in seconds
+  tags = [],
 }: {
   query: string;
   params?: QueryParams;
   revalidate?: number | false;
+  tags?: string[];
 }) {
   return client.fetch<QueryResponse>(query, params, {
     next: {
       revalidate: tags.length ? false : revalidate,
+      tags,
     },
   });
 }
