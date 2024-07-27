8786e087d10eb1447f53bb4326da718a5d158c4b
SLMCreatives <sulaiman@slmcreatives.com>
2024-07-28 03:48:58 +0800

    chatbox

--- app/lab/chatbot/page.tsx
+++ app/lab/chatbot/page.tsx
@@ -3,59 +3,80 @@
 import { useState } from "react";
 import { Message, continueConversation } from "../../action";
 import { readStreamableValue } from "ai/rsc";
-
+import { Input } from "Sulaiman/S/components/ui/input";
+import { Button } from "Sulaiman/S/components/ui/button";
+import Form from "Sulaiman/app/form/page";
+import { Textarea } from "Sulaiman/S/components/ui/textarea";
+import { ThemeToggle } from "../minder/ThemeToggle";
 // Force the page to be dynamic and allow streaming responses up to 30 seconds
 export const dynamic = "force-dynamic";
 
 export default function Chatbot() {
   const [conversation, setConversation] = useState<Message[]>([]);
   const [input, setInput] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   return (
-    <main>
-      <div className="flex p-6 justify-center bg-black">
-        <h1 className="text-white font- text-4xl">Gemini Chatbot</h1>
+    <main className="bg-slate-50 min-w-0 flex">
+      <ThemeToggle />
+      <div className=" p-6 justify-center bg-slate-100 dark:bg-slate-950 text-slate-800 dark:text-slate-50">
+        <h1 className="text-4xl ">Gemini Chatbot</h1>
       </div>
-      <div className="flex flex-col [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)] h-screen items-center">
-        <div className="flex flex-col gap-4 lg:p-4 p-2 text-white w-[70vw]">
+      <div className="w-[2px] dark:bg-slate-600 bg-gray-200"></div>
+      <div className="flex flex-col h-screen w-full bg-slate-100 dark:bg-slate-950 text-slate-800 dark:text-slate-50 px-10">
+        <div className="lg:p-4 p-2 py-12 overflow-y-auto scrollbar-thumb-slate-400 scrollbar-thin">
+          {conversation.length === 0 ? (
+            <em className="opacity-50">Ask me anything...</em>
+          ) : null}
           {conversation.map((message, index) => (
-            <div key={index} className="grid grid-cols-4 overflow-y-scroll">
-              <p className="font-semibold capitalize">{message.role}:</p>
-              <p className="text-md col-span-3">{message.content}</p>
+            <div key={index} className="grid grid-cols-6 gap-3 p-1">
+              <p className="capitalize text-right">{message.role}:</p>
+              <code className="text-md col-span-5 pl-3">{message.content}</code>
             </div>
           ))}
         </div>
-        <div className="fixed bottom-5 flex gap-4 justify-center w-[100vw]">
-          <input
-            className="border rounded p-2 w-[70vw]"
-            type="text"
+        <div className="flex flex-col p-4 w-full gap-4 justify-center sticky bottom-2 ">
+          <Textarea
+            name="message"
+            id="message"
+            rows={5}
+            placeholder="Type something..."
+            className="border rounded-lg p-3 bg-slate-100 text-slate-800 dark:bg-slate-600 dark:text-slate-200"
             value={input}
             onChange={(event) => {
               setInput(event.target.value);
             }}
-          />
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            onClick={async () => {
-              const { messages, newMessage } = await continueConversation([
-                ...conversation,
-                { role: "user", content: input },
-              ]);
-              let textContent = "";
-              for await (const text of readStreamableValue(newMessage)) {
-                textContent = `${textContent}${text}`;
-                setConversation([
-                  ...messages,
-                  { role: "assistant", content: textContent },
-                ]);
-                setInput("");
-              }
+            onKeyDown={(event) => {
+              if (event.key === "Enter") event.preventDefault(), ChatbotReq()();
             }}
+          />
+          <Button
+            className=" text-white bg-blue-500 hover:bg-blue-700 font-bold py-2 px-4 rounded"
+            onClick={ChatbotReq()}
+            disabled={isLoading}
           >
-            Send Message
-          </button>
+            {isLoading ? "Loading..." : "Send"}
+          </Button>
         </div>
       </div>
     </main>
   );
+
+  function ChatbotReq() {
+    return async () => {
+      const { messages, newMessage } = await continueConversation([
+        ...conversation,
+        { role: "user", content: input },
+      ]);
+      let textContent = "";
+      for await (const text of readStreamableValue(newMessage)) {
+        textContent = `${textContent}${text}`;
+        setConversation([
+          ...messages,
+          { role: "assistant", content: textContent },
+        ]);
+        setInput("");
+      }
+    };
+  }
 }
--- app/lab/minder/page.tsx
+++ app/lab/minder/page.tsx
@@ -11,9 +11,7 @@ import {
   ChevronsUpDown,
   ExternalLinkIcon,
   LinkIcon,
-  Moon,
   PopcornIcon,
-  Sun,
 } from "lucide-react";
 import { TrendingUp } from "lucide-react";
 import {
@@ -33,13 +31,6 @@ import {
   CardTitle,
 } from "../../../S/components/ui/card";
 import { ChartConfig, ChartContainer } from "../../../S/components/ui/chart";
-import { useTheme } from "next-themes";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "Sulaiman/S/components/ui/dropdown-menu";
 import { Button } from "Sulaiman/S/components/ui/button";
 import {
   Popover,
@@ -58,6 +49,7 @@ import {
   CommandList,
 } from "Sulaiman/S/components/ui/command";
 import { cn } from "Sulaiman/S/lib/utils";
+import { ThemeToggle } from "./ThemeToggle";
 
 const initialItems = [0, 1, 2, 3, 4];
 const height = 70;
@@ -506,38 +498,6 @@ function Component(rating: any) {
         </ChartContainer>
       </CardContent>
     </Card>
-  );
-}
-function ThemeToggle() {
-  const { setTheme } = useTheme();
-  return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button
-          variant="default"
-          size="icon"
-          className="absolute top-4 right-4 w-[1.2rem] h-[1.2rem]"
-        >
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 text-slate-900 bg-white ring-0 focus:none" />
-          <Moon className="absolute  w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 text-slate-200 bg-black  focus:none" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent
-        align="end"
-        className="dark:bg-slate-900 bg-white p-2 flex flex-col gap-2 rounded-md absolute isolate top-2 right-2"
-      >
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
   );
 }
 function RatingToggle(rating: any) {
