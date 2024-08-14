6efdeaef689590755ed4065efae35a9c8b8f27d9
SLMCreatives <sulaiman@slmcreatives.com>
2024-08-14 17:52:03 +0800

    fix nav

--- app/_components/NewHeader.tsx
+++ app/_components/NewHeader.tsx
@@ -22,7 +22,6 @@ const navigation = [
   { name: "Web Dev", href: "/webdev" },
   { name: "Portfolio", href: "/portfolio" },
   { name: "Blog", href: "/blog" },
-  { name: "Contact", href: "/contact" },
 ];
 
 export default function NewHeader() {
@@ -51,50 +50,12 @@ export default function NewHeader() {
           </div>
           <div className="flex flex-row items-center gap-6">
             <ThemeToggle />
-            <Popover>
-              <PopoverTrigger>
-                <Avatar className="w-10 h-10 cursor-pointer hover:ring-1 hover:ring-slate-400">
-                  <AvatarImage src="/sulaiman.jpg"></AvatarImage>
-                  <AvatarFallback>SS</AvatarFallback>
-                </Avatar>
-              </PopoverTrigger>
-              <PopoverContent
-                className="pl-3 pr-0 py-1 w-fit rounded-xl drop-shadow-xl shadow-cyan-400/50 dark:bg-black"
-                align="start"
-                alignOffset={30}
-                side="left"
-              >
-                <section className="w-32 p-1">
-                  <p className="font-bold text-lg">Get in Touch:</p>
-                  <div className="grid grid-flow-row grid-rows-4 text-right">
-                    <Link href="https://twitter.com/SulaimanShafiq">
-                      <p className="font-normal text-sm mt-2 flex gap-4 justify-between hover:font-bold pr-2 hover:text-cyan-400">
-                        X
-                        <XIcon className="w-6 h-6 inline rounded-full ml-4" />
-                      </p>
-                    </Link>
-                    <Link href="https://www.facebook.com/slmcreatives.my/">
-                      <p className="font-normal text-sm mt-2 flex justify-between hover:font-bold pr-2 hover:text-cyan-400">
-                        Facebook
-                        <FacebookIcon className="w-6 h-6 inline rounded-full ml-4" />
-                      </p>
-                    </Link>
-                    <Link href="https://wa.me/601121292383">
-                      <p className="font-normal text-sm mt-2 flex justify-between hover:font-bold pr-2 hover:text-cyan-400">
-                        Whatsapp
-                        <WhatsappIcon className="w-6 h-6 inline rounded-full ml-4" />
-                      </p>
-                    </Link>
-                    <Link href={"mailto:sulaiman@slmcreatives.com"}>
-                      <p className="font-normal text-sm mt-2 flex justify-between hover:font-bold pr-2 hover:text-cyan-400">
-                        Email
-                        <EmailIcon className="w-6 h-6 inline rounded-full ml-4" />
-                      </p>
-                    </Link>
-                  </div>
-                </section>
-              </PopoverContent>
-            </Popover>
+            <Link href="/contact">
+              <Avatar className="w-10 h-10 cursor-pointer hover:ring-1 hover:ring-slate-400">
+                <AvatarImage src="/sulaiman.jpg"></AvatarImage>
+                <AvatarFallback>SS</AvatarFallback>
+              </Avatar>
+            </Link>
           </div>
         </div>
       </section>
